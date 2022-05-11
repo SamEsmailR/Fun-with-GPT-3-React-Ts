74cf5911c70db8e08a2258b9ee3bf9b6ecf9bcd6
marinakosova <marina.kosova@gmail.com>
2022-05-11 11:41:57 -0700

    add Header styles

---
+++ src/components/Header.styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+    .header {
+        color: #FFF;
+        font-size: 50px;
+        padding-bottom: 40px;
+    }
+`;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { HeaderContainer } from './Header.styles';
 
-function Header() {
+function Header(){
     return (
-        <header>
-            Fun with GPT-3
-        </header>
+        <HeaderContainer>
+            <h1 className='header'>Fun with GPT-3</h1>
+        </HeaderContainer>
     );
-  }
+}
+
+export default Header;
+
   
-  export default Header;
+
   
